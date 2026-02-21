4981e44814e9f69240274b41a82c0ace66acf122
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2026-02-21 11:25:27 -0300

    ingestao de dados

---
+++ zenon-fraud-detector/src/main/java/br/com/zenon/TransactionIngestor.java
@@ -0,0 +1,42 @@
+package br.com.zenon;
+
+import java.math.BigDecimal;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class TransactionIngestor {
+
+  public List<Transaction> read(String filename) {
+    Path path = Path.of(filename);
+    try {
+      List<String> lines = Files.readAllLines(path);
+      return lines.stream()
+          .skip(1)
+          .limit(1000)
+          .map(this::parseTransaction)
+          .toList();
+    } catch (Exception ex) {
+      throw new RuntimeException("Erro ao ler o arquivo: " + filename, ex);
+    }
+  }
+
+  private Transaction parseTransaction(String line) {
+    String[] chunks = line.split(",");
+
+    int step = Integer.parseInt(chunks[0]);
+    TransactionType type = TransactionType.valueOf(chunks[1]);
+
+    BigDecimal amount = new BigDecimal(chunks[2]);
+
+    var origin = new TransactionCustomer(chunks[3], new BigDecimal(chunks[4]), new BigDecimal(chunks[5]));
+    var recipient = new TransactionCustomer(chunks[6], new BigDecimal(chunks[7]), new BigDecimal(chunks[8]));
+
+    boolean isFraud = "1".equals(chunks[9]);
+
+    boolean isFlaggedFraud = "1".equals(chunks[10]);
+
+    return new Transaction(step, type, amount, origin, recipient, isFraud, isFlaggedFraud);
+  }
+
+}
